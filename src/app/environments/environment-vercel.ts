c4159a460e8f776eb4e99cf20fdce68ce31e0e3f
Mariano Santos <mariano veron>
2026-01-13 12:55:25 -0300

    fix enviroment

---
+++ src/app/environments/environment-vercel.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  serviceId: process.env['SERVICEID'] || '',
+  templateId: process.env['TEMPLATEID'] || '',
+  authKey: process.env['AUTHKEY'] || '',
+  openAiKey: process.env['OPENAI_KEY'] || '',
+};
